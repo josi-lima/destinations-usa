c22e3ca115ad04a6a5acd41dd6ea6e1de1c99463
josi-lima <teacher.josi@yahoo.com>
2022-09-30 17:14:59 -0300

    add files

---
+++ script.js
@@ -0,0 +1,3 @@
+// Data base with the states' and cities' names
+
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
